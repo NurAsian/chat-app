5ee7cfbcd3d748b8ec4f6850e52df30a40aeb211
NurAsian <74744849+NurAsian@users.noreply.github.com>
2023-02-28 08:29:55 +0700

    Update config.js

--- scripts/main/config.js
+++ scripts/main/config.js
@@ -1,13 +1,26 @@
 // ISI DENGAN PROJECT FIREBASE MILIK TEMEN-TEMEN SENDIRI
+// const firebaseConfig = {
+//     apiKey: "PUNYA-KAMU",
+//     authDomain: "PUNYA-KAMU",
+//     databaseURL: "PUNYA-KAMU",
+//     projectId: "PUNYA-KAMU",
+//     storageBucket: "PUNYA-KAMU",
+//     messagingSenderId: "PUNYA-KAMU",
+//     appId: "PUNYA-KAMU"
+// };
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "PUNYA-KAMU",
-    authDomain: "PUNYA-KAMU",
-    databaseURL: "PUNYA-KAMU",
-    projectId: "PUNYA-KAMU",
-    storageBucket: "PUNYA-KAMU",
-    messagingSenderId: "PUNYA-KAMU",
-    appId: "PUNYA-KAMU"
+  apiKey: "AIzaSyA4IkkkFzMEXxx3AxkPP57R6K5axHvPPcY",
+  authDomain: "kopi-chat-dc910.firebaseapp.com",
+  databaseURL: "https://kopi-chat-dc910-default-rtdb.firebaseio.com",
+  projectId: "kopi-chat-dc910",
+  storageBucket: "kopi-chat-dc910.appspot.com",
+  messagingSenderId: "727350594055",
+  appId: "1:727350594055:web:ab0df3a61e40ddfe3cb8a2"
 };
+
+
 
 // BIAR GA KELAMAAN PAS MAU PANGGIL NANTI
 const app = firebase.initializeApp(firebaseConfig);
